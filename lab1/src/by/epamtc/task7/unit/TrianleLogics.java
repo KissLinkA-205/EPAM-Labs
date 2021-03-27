9d89c606f2312f8d68b80a8011b0e2e28205b5f3
Анжелика <71188712+KissLinkA-205@users.noreply.github.com>
2021-03-27 09:55:49 +0300

    Delete lab1/src directory

--- lab1/src/by/epamtc/task7/unit/TrianleLogics.java
+++
@@ -1,9 +0,0 @@
-package by.epamtc.task7.unit;
-
-import static java.lang.Math.sqrt;
-
-public class TrianleLogics {
-    public static double distanceToOrigin(double x, double y) {
-        return sqrt(x * x + y * y);
-    }
-}
