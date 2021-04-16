8e168696abc1f67224f7301e4f60cc26ae48a6b4
Анжелика Деркач <71188712+KissLinkA-205@users.noreply.github.com>
2021-04-16 23:03:07 +0300

    Delete lab1/src directory

--- lab1/src/by/epamtc/task1/unit/FindLogics.java
+++
@@ -1,8 +0,0 @@
-package by.epamtc.task1.unit;
-
-public class FindLogics {
-    public static int findLastDigit(int number) {
-        int ten = 10;
-        return number - (number / ten) * ten;
-    }
-}
