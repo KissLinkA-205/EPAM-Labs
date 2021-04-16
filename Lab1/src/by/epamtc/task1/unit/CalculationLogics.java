d29a20a78a768affea584ad1da6f36c114ede322
Анжелика Деркач <71188712+KissLinkA-205@users.noreply.github.com>
2021-04-16 23:05:51 +0300

    Delete Lab1 directory

--- Lab1/src/by/epamtc/task1/unit/CalculationLogics.java
+++
@@ -1,7 +0,0 @@
-package by.epamtc.task1.unit;
-
-public class CalculationLogics {
-    public static int calculateSquare(int number) {
-        return number * number;
-    }
-}
--- Lab1/src/by/epamtc/task2/unit/YearLogics.java
+++
@@ -1,7 +0,0 @@
-package by.epamtc.task2.unit;
-
-public class YearLogics {
-    public static boolean isLeapYear(int year) {
-        return year % 4 == 0;
-    }
-}
