09c084644102cf8f4152322e2896307526dc4baa
Анжелика Деркач <71188712+KissLinkA-205@users.noreply.github.com>
2021-04-17 17:40:33 +0300

    Refactoring lab2

---
+++ lab2/src/by/epamtc/task/entity/Colour.java
@@ -0,0 +1,20 @@
+package by.epamtc.task.entity;
+
+public enum Colour{
+    RED("Красный"),
+    ORANGE("Оранжевый"),
+    YELLOW("Жёлтый"),
+    GREEN("Зеленый"),
+    BLUE("Синий"),
+    PURPLE("Фиолетовый"),
+    COLOURLESS("Бесцветный");
+    private final String colour;
+
+    Colour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getColour() {
+        return this.colour;
+    }
+}
---
+++ lab2/src/by/epamtc/task/run/Runner.java
@@ -0,0 +1,55 @@
+package by.epamtc.task.run;
+
+import by.epamtc.task.entity.Ball;
+import by.epamtc.task.entity.Basket;
+import by.epamtc.task.entity.Colour;
+import by.epamtc.task.logic.BasketLogics;
+import by.epamtc.task.logic.InputLogics;
+
+public class Runner {
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        System.out.println("Ввод карзины:");
+        double weight;
+        Colour colour;
+        System.out.println("Введите параметры 1 шарика");
+        Ball ball1 = inputBall();
+        System.out.println("Введите параметры 2 шарика");
+        Ball ball2 = inputBall();
+        System.out.println("Введите параметры 3 шарика");
+        Ball ball3 = inputBall();
+
+        basket.putBall(ball1);
+        basket.putBall(ball2);
+        basket.putBall(ball3);
+
+        System.out.println("Полученная корзина:");
+        System.out.print(basket);
+
+        weight = BasketLogics.calculateWeight(basket);
+        System.out.printf("\nВес всех шариков в корзине: %.3f", weight);
+        int count = BasketLogics.countBallsByColour(basket, Colour.BLUE);
+        System.out.println("\nКоличество синих шариков в корзине: " + count);
+    }
+
+    public static Ball inputBall() {
+        Ball ball = new Ball();
+        double weight;
+        Colour colour;
+        System.out.println("Введите вес шарика: ");
+        weight = InputLogics.inputWeight();
+        ball.setWeight(weight);
+
+        System.out.println("Введите цвет шарика: " +
+                "\n1 - красный" +
+                "\n2 - оранжевый" +
+                "\n3 - желтый" +
+                "\n4 - зеленый" +
+                "\n5 - синий" +
+                "\n6 - фиолетовый");
+        colour = InputLogics.inputColour();
+        ball.setColour(colour);
+
+        return ball;
+    }
+}
