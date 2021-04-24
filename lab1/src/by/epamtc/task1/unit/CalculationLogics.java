ca10f6061f895a04cd5b2e93e24cf5bdf59cdf92
Анжелика Деркач <71188712+KissLinkA-205@users.noreply.github.com>
2021-04-24 19:33:49 +0300

    Delete lab1/src/by/epamtc directory

--- lab1/src/by/epamtc/task1/unit/CalculationLogics.java
+++
@@ -1,7 +0,0 @@
-package by.epamtc.task1.unit;
-
-public class CalculationLogics {
-    public int calculateSquare(int number) {
-        return number * number;
-    }
-}
--- lab1/src/by/epamtc/task1/unit/FindLogics.java
+++
@@ -1,8 +0,0 @@
-package by.epamtc.task1.unit;
-
-public class FindLogics {
-    public int findLastDigit(int number) {
-        int ten = 10;
-        return number - (number / ten) * ten;
-    }
-}
