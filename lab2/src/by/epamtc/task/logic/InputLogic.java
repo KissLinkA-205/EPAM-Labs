c2c2b12d5c1e5d7c3d1ff786080fcc38bfd6760b
Анжелика Деркач <71188712+KissLinkA-205@users.noreply.github.com>
2021-04-24 19:35:45 +0300

    Refactoring lab2

---
+++ lab2/src/by/epamtc/task/logic/InputLogic.java
@@ -0,0 +1,33 @@
+package by.epamtc.task.logic;
+
+import by.epamtc.scanner.DataScanner;
+import by.epamtc.task.entity.Colour;
+
+public class InputLogic {
+    public double inputWeight() {
+        double weight;
+        do {
+            weight = DataScanner.enterDoubleFromConsole();
+        } while (weight < 0);
+        return weight;
+    }
+
+    public Colour inputColour() {
+        int key;
+        do {
+            key = DataScanner.enterIntFromConsole();
+        } while (key < 1 || key > 6);
+
+        Colour colour = switch (key) {
+            case 1 -> Colour.RED;
+            case 2 -> Colour.ORANGE;
+            case 3 -> Colour.YELLOW;
+            case 4 -> Colour.GREEN;
+            case 5 -> Colour.BLUE;
+            case 6 -> Colour.PURPLE;
+            default -> Colour.COLOURLESS;
+        };
+
+        return colour;
+    }
+}
