73fe2332a63bbab0531b9f401eb4b084024aeaf1
Анжелика Деркач <71188712+KissLinkA-205@users.noreply.github.com>
2021-05-09 20:36:53 +0300

    Delete lab3/src/by/epamtc directory

--- lab3/src/by/epamtc/task2/service/SortJaggedArray.java
+++
@@ -1,7 +0,0 @@
-package by.epamtc.task2.service;
-
-import by.epamtc.exeptions.EmptyArrayException;
-
-public interface SortJaggedArray {
-    void bubbleSort(int[][] jaggedArray, boolean increasing) throws EmptyArrayException;
-}
